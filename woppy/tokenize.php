341bef57b43b46a3506a8fe5046aa71624f5eb02
Antonio Dell'Elce <antouk@dellelce.com>
2019-03-12 15:11:42 +0000

    initial test of token_get_all

--- woppy/tokenize.php
+++ woppy/tokenize.php
@@ -10,10 +10,18 @@ function tokenize($buf)
  {
   if (is_array($token))
   {
-   echo "Line {$token[2]}: ", token_name($token[0]), " ('{$token[1]}')", PHP_EOL;
+   //echo "Line {$token[2]}: ", token_name($token[0]), " ('{$token[1]}')", PHP_EOL;
+   echo $token[2].";".$token[0].";".$token[1].PHP_EOL;
   }
  }
 }
+
+function tokenize_file($name)
+{
+ tokenize(file_get_contents($name));
+}
+
+tokenize_file("woppy/tokenize.php");
 
 
 // EOF
